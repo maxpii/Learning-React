62989e30d92e37d1ceb3db12c172de818d1d4683
Muhammad Raza <hmrazarafique@gmail.com>
2024-06-01 21:27:41 -0400

    Learned about props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div> <ListGroup></ListGroup> </div>;
+  let items = [
+    {"name":"New York",
+        "id":1,
+        "population":"A lot"
+    },
+    {
+        "name": "Pakistan",
+        "id":2,
+        "population":"Very very much"
+    }
+];
+
+  const handleItem = (item: string) => {
+    console.log(item);
+  }
+  return <div> 
+      <ListGroup items={items} heading={"Cities"} onSelectItem={handleItem}/>
+     </div>;
 }
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,30 +1,28 @@
 import { MouseEvent, useState }  from "react";
-function ListGroup() {
+interface Props {
+  items: any[];
+  heading: string;
+  onSelectItem: (item:string) => void;
+}
+
+function ListGroup({items ,heading,onSelectItem}: Props) {
     // going to use list mapping, similar to mappping and list comprehension in python
-    let items = [
-        {"name":"New York",
-            "id":1,
-            "population":"A lot"
-        },
-        {
-            "name": "Pakistan",
-            "id":2,
-            "population":"Very very much"
-        }
-    ];
     // hook, taps into built in react features
     const [selectedIndex,setSelectedIndex] = useState(-1); // array, first index variable second is updater function
     const [name,setName] = useState(""); 
 
   return (
     <>
-    <h1>List</h1>
+    <h1>{heading}</h1>
     {items.length == 0  && <p>No item found</p>/* More concise way to write this
     If the condition is true, it returns paragraph, else nothing
     Common technique*/ }
       <ul className="list-group">
         {items.map((item, index) => ( 
-            <li key={item.id} onClick={() => setSelectedIndex(index)} 
+            <li key={item.id} onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }} 
                 className={selectedIndex == index ? "list-group-item active": "list-group-item"}>{item.name} has a population
                 of {item.population} 
             </li>
